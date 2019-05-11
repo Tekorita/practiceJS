7343972029340fd06d76d73cdc9fd9d4db9f3c1e
David Palma <davidpalmalugo@gmail.com>
2019-05-11 19:22:42 -0400

    variables y tipos de datos

---
+++ js/variablesTiposDeDatos.js
@@ -0,0 +1,39 @@
+///////////////////VARIABLES//////////////////////////////////////
+//1 Declaracion
+
+var gatoGrande;
+
+//2 Inicializacion
+
+gatoGrande = "esto es un gato";
+
+//3 Utilizacion
+
+console.log(gatoGrande);
+
+//4 Declaracion + inicializacion + utilizacion
+
+var gatoPequeño = "miau";
+
+console.log(gatoPequeño);
+
+//5 Asignacion de una variable a otra
+
+var felino = gatoGrande;
+
+console.log(felino);
+
+//6 Constantes - no se pueden cmabiar
+
+const PI = 3.1416;
+
+/////////////////////TIPO DE DATOS//////////////////////////////
+
+var cadena = "arbol";
+var numero = 30;
+var real = 20.6;
+var booleano = true;
+
+console.log(PI); // console.log se escribe en 
+
+document.write(PI); // document.write escribe en html
