c2e2bf25e9fa6db1c8027559cb71dd5b32d99a66
David Palma <davidpalmalugo@gmail.com>
2019-05-13 19:18:08 -0400

    cuadros de dialogos y ventanas emergentes

---
+++ js/cuadrosDialogosVentanasEmergentes.js
@@ -0,0 +1,32 @@
+/*
+    alert
+    confirm
+    prompt
+*/
+
+//var saludos = "buenas noches ";
+
+//alert(saludos+"a todos \n como estan?");
+
+//a = confirm("Necesita ayuda?");
+
+/*
+if(a){
+    document.write("dijo que si");
+}else{
+    document.write("dijo que no");
+}*/
+
+//if(a){window.location="ayuda.html"}
+
+var a = prompt("Ingrese su nombre completo: ");
+
+//document.write("hola "+a);
+
+if(a){
+    document.write("hola " + a);
+}else if(a==null){
+    document.write("usted le dio a cancelar");    
+}else{
+    document.write("Usted no ingreso nada")
+}
